6de31802fa6c053858d104f03e5bf0d1e3b68244
jbmenard <jbaptiste.menard@gmail.com>
2021-08-05 11:06:53 +0200

    add-auth and modify user-controller

--- itinelib/Api/controllers/user-controller.js
+++ itinelib/Api/controllers/user-controller.js
@@ -1,7 +1,60 @@
 const db = require('../models');
 const User = db.User;
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 class UserController {
+
+    register = async (req, res) => {
+        try {
+            const email = req.body.email;
+            let user = await User.findOne({where: {email}});
+            if(user) {
+                res.status(400).send('Utilisateur déjà existant')
+            } else {
+                const password = await bcrypt.hash(req.body.password, 10);
+                user = {email, password};
+                // = user { email : email, password : password }
+                const data  = await User.create(user)
+                res.json({email: data.email, createdAt: data.createdAt});
+            }
+        } catch (err)  {
+            console.err(err);
+            res.status(400).send('Mauvaise requête');
+        }
+    }
+
+    login = async (req, res) => {
+        let email, password;
+         try {
+             email = req.body.email;
+             password = req.body.password;
+         } catch (err) {
+             res.status(400).send('Mauvais body');
+         }
+
+         const user = await User.findOne({where: {email}});
+         if(!user) {
+             res.status(401).send(`Erreur d'authentification`);
+         } else {
+            const isValid = await bcrypt.compare(password, user.password);
+            if(!isValid) {
+                res.status(401).send(`Erreur d'authentification`);
+            } else {
+                const token = jwt.sign(
+                    {userId: user.id},
+                    'Faire_De_La_Rando_Avec_Un_Sac_A_Dos',
+                    {expiresIn: '24h'}
+                );
+
+                res.json({
+                    id: user.id,
+                    email: user.email,
+                    token
+                })
+            }
+         }
+    }
 
     getAll = async (req, res) => {
         const data = await User.findAll();
