4829c06af7bc00ce6c31c1c1fffb15a602369fd6
Luco06 <luc-olivier.yohan@hotmail.fr>
2021-08-23 16:52:21 +0200

    add heroku

--- itinelib/front/src/components/Register/Register.jsx
+++ itinelib/front/src/components/Register/Register.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import './register.scss';
-import { Header, Button } from '../index';
+import { Button } from '../index';
 import logo from '../../assets/images/logo.png'
 
 export default class Register extends React.Component {
