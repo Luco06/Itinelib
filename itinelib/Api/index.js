2ea31e1e273a5834e855b8f0f4a5243ed40ba7a7
Luco06 <luc-olivier.yohan@hotmail.fr>
2021-08-26 13:35:50 +0200

    modif config

--- itinelib/Api/index.js
+++ itinelib/Api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 3200;
+const port = 3200;
 const router = require('./routes/index'); 
 
 app.use(cors());
